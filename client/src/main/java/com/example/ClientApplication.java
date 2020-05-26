7210e3cc4f2c2946cb0427eb98df34e8e120a4f8
chenfei <luckxiaofei@outlook.com>
2020-05-26 10:24:51 +0800

    [功能]:集成nacos配置中心 [进度]:完成

--- client/src/main/java/com/example/ClientApplication.java
+++ client/src/main/java/com/example/ClientApplication.java
@@ -1,16 +1,14 @@
 package com.example;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
 import com.example.demo.HelloService;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
-
-import com.alibaba.dubbo.config.annotation.Reference;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableDubbo
 @EnableNacosConfig
@@ -19,7 +17,7 @@ public class ClientApplication {
 
     @Reference(version = "1.0.0")
     private HelloService demoService;
-    @NacosValue(value = "test",autoRefreshed = true)
+    @NacosValue(value = "test", autoRefreshed = true)
     private String test;
 
     public static void main(String[] args) {
@@ -30,6 +28,7 @@ public class ClientApplication {
     @PostConstruct
     public void init() {
         String sayHello = demoService.sayHello("world");
-        System.err.println(sayHello + test);
+        System.err.println(sayHello);
+        System.err.println(test);
     }
 }
