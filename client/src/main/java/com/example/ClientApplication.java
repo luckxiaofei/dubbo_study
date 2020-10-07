97bd19f8869f6355bb91813937cc73eca3dbc62f
chenfei <luckxiaofei@outlook.com>
2020-10-07 11:05:00 +0800

    优化注解

--- client/src/main/java/com/example/ClientApplication.java
+++ client/src/main/java/com/example/ClientApplication.java
@@ -1,21 +1,24 @@
 package com.example;
 
-import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
 import com.example.demo.HelloService;
+import org.apache.dubbo.config.annotation.Reference;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
 
+/**
+ * @author fei
+ */
 @EnableDubbo
 @EnableNacosConfig
 @SpringBootApplication
 public class ClientApplication {
 
-    @Reference(version = "1.0.0")
+    @Reference
     private HelloService demoService;
     @NacosValue(value = "test", autoRefreshed = true)
     private String test;
